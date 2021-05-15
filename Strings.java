7839cfc21047d5eb4b85ea7b7a06e0d3302369b7
Aliuken <alber84ou@gmail.com>
2021-05-15 17:18:51 +0200

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,58 @@
+package java.util;
+
+import java.util.function.Supplier;
+
+public final class Strings {
+    public static final String EMPTY_STRING = "";
+
+    private Strings() {
+        throw new AssertionError("No java.util.Strings instances for you!");
+    }
+
+    public static boolean equalsIgnoreCase(String a, String b) {
+        return (a == b) || (a != null && a.equalsIgnoreCase(b));
+    }
+
+    public static String requireNonNullNorEmpty(String str) {
+        if (str == null)
+            throw new NullPointerException();
+        if (EMPTY_STRING.equals(str))
+            throw new IllegalArgumentException();
+        return str;
+    }
+
+    public static String requireNonNullNorEmpty(String str, String message) {
+        if (str == null)
+            throw new NullPointerException(message);
+        if (EMPTY_STRING.equals(str))
+            throw new IllegalArgumentException(message);
+        return str;
+    }
+
+    public static boolean isNullOrEmpty(String str) {
+        return (str == null || EMPTY_STRING.equals(str));
+    }
+
+    public static boolean nonNullNorEmpty(String str) {
+        return (str != null && !EMPTY_STRING.equals(str));
+    }
+
+    public static String requireNonNullNorEmptyElse(String str, String defaultStr) {
+        return (str != null && !EMPTY_STRING.equals(str)) ? str : requireNonNullNorEmpty(defaultStr, "defaultStr");
+    }
+
+    public static String requireNonNullNorEmptyElseGet(String str, Supplier<String> supplier) {
+        return (str != null && !EMPTY_STRING.equals(str)) ? str
+                : requireNonNullNorEmpty(Objects.requireNonNull(supplier, "supplier").get(), "supplier.get()");
+    }
+
+    public static String requireNonNullNorEmpty(String str, Supplier<String> messageSupplier) {
+        if (str == null)
+            throw new NullPointerException(messageSupplier == null ?
+                    null : messageSupplier.get());
+        if (EMPTY_STRING.equals(str))
+            throw new IllegalArgumentException(messageSupplier == null ?
+                    null : messageSupplier.get());
+        return str;
+    }
+}
