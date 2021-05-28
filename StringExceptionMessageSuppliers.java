7fbc80b08a3460396f5a59d554548708c62da721
Aliuken <alber84ou@gmail.com>
2021-05-28 21:01:40 +0200

    Update StringExceptionMessageSuppliers.java

--- StringExceptionMessageSuppliers.java
+++ StringExceptionMessageSuppliers.java
@@ -4,18 +4,6 @@ import java.util.Objects;
 import java.util.function.Supplier;
 
 public record StringExceptionMessageSuppliers(Supplier<String> nullStringParam, Supplier<String> emptyStringParam, Supplier<String> whitespaceStringParam) implements StringExceptionRecordGetter<Supplier<String>, Supplier<String>, Supplier<String>>, StringExceptionThrower<NullPointerException, IllegalArgumentException, IllegalArgumentException> {
-    private String nullStringExceptionMessage() {
-        return Objects.requireNonNull(nullStringParam, "stringExceptionMessageSuppliers.nullStringParam").get();
-    }
-
-    private String emptyStringExceptionMessage() {
-        return Objects.requireNonNull(emptyStringParam, "stringExceptionMessageSuppliers.emptyStringParam").get();
-    }
-
-    private String whitespaceStringExceptionMessage() {
-        return Objects.requireNonNull(whitespaceStringParam, "stringExceptionMessageSuppliers.whitespaceStringParam").get();
-    }
-
     @Override
     public void throwNullStringException() throws NullPointerException {
         throw new NullPointerException(Objects.requireNonNull(nullStringExceptionMessage(), "stringExceptionMessageSuppliers.nullStringParam.get()"));
@@ -29,5 +17,17 @@ public record StringExceptionMessageSuppliers(Supplier<String> nullStringParam, 
     @Override
     public void throwWhitespaceStringException() throws IllegalArgumentException {
         throw new IllegalArgumentException(Objects.requireNonNull(whitespaceStringExceptionMessage(), "stringExceptionMessageSuppliers.whitespaceStringParam.get()"));
+    }
+
+    private String nullStringExceptionMessage() {
+        return Objects.requireNonNull(nullStringParam, "stringExceptionMessageSuppliers.nullStringParam").get();
+    }
+
+    private String emptyStringExceptionMessage() {
+        return Objects.requireNonNull(emptyStringParam, "stringExceptionMessageSuppliers.emptyStringParam").get();
+    }
+
+    private String whitespaceStringExceptionMessage() {
+        return Objects.requireNonNull(whitespaceStringParam, "stringExceptionMessageSuppliers.whitespaceStringParam").get();
     }
 }
