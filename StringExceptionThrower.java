5c75bd1e9022ca888e4ffacd79997763fd3c0f66
Aliuken <alber84ou@gmail.com>
2021-05-30 19:49:28 +0200

    Delete StringExceptionThrower.java

--- StringExceptionThrower.java
+++
@@ -1,8 +0,0 @@
-package myjava.util;
-
-public interface StringExceptionThrower<E1 extends Throwable, E2 extends Throwable, E3 extends Throwable> {
-    void throwNullStringException() throws E1;
-    void throwEmptyStringException() throws E2;
-    void throwWhitespaceStringException() throws E3;
-}
-
